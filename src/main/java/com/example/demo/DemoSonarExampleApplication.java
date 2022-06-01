12c3b1e5231ea82413319e5ae9bd051c81a937d7
Jilani-s <106386175+Jilani-s@users.noreply.github.com>
2022-06-01 20:42:04 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/DemoSonarExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSonarExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSonarExampleApplication.class, args);
+	}
+
+}
